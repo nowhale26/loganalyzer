bfaf9b33cd2b07ea7afce5997347930e6422ebb0
Nikita <makhno.nikita26@gmail.com>
2024-11-12 17:36:15 +0300

    feat: Добавлен класс для хранения полей лога

---
+++ src/main/java/backend/academy/Log.java
@@ -0,0 +1,19 @@
+package backend.academy;
+
+import lombok.Getter;
+import java.time.LocalDateTime;
+
+@Getter
+public class Log {
+    private final LocalDateTime dateTime;
+    private final int responseCode;
+    private final int responseSize;
+    private final String resource;
+
+    public Log(LocalDateTime dateTime, int responseCode, int responseSize, String resource){
+        this.dateTime=dateTime;
+        this.responseCode=responseCode;
+        this.responseSize=responseSize;
+        this.resource=resource;
+    }
+}
