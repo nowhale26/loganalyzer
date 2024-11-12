568f114352d8969b93c8935a77eb63abae2a00c0
Nikita <makhno.nikita26@gmail.com>
2024-11-12 17:16:23 +0300

    fix: Добавлена проверка, что передан обязательный параметр path

--- src/main/java/backend/academy/ArgsParser.java
+++ src/main/java/backend/academy/ArgsParser.java
@@ -8,22 +8,26 @@ import java.util.Map;
 public class ArgsParser {
     private LocalDateTime from;
     private LocalDateTime to;
-    private String logfiles;
+    private String logfile;
     private String format;
     private Map<String, String> arguments;
 
-    private Map<String, String> parseArguments(String[] args){
+    private Map<String, String> parseArguments(String[] args) {
         Map<String, String> argsMap = new HashMap<>();
-        for(int i=0;i<args.length;i+=2){
-            argsMap.put(args[i].replace("--",""),args[i+1]);
+        for (int i = 0; i < args.length; i += 2) {
+            argsMap.put(args[i].replace("--", ""), args[i + 1]);
         }
         return argsMap;
     }
 
-    private void initializeParametrs(){
+    private void initializeParametrs() {
         from = arguments.containsKey("from") ? LocalDateTime.parse(arguments.get("from")) : null;
         to = arguments.containsKey("to") ? LocalDateTime.parse(arguments.get("to")) : null;
-        logfiles = arguments.get("path");
-        format = arguments.getOrDefault("format","markdown");
+        if (arguments.containsKey("path")) {
+            logfile = arguments.get("path");
+        } else {
+            System.err.println("Не указан обязательный параметр --path");
+        }
+        format = arguments.getOrDefault("format", "markdown");
     }
 }
