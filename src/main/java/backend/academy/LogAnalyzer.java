ede41d947f4b85510d001fadf6b7c68da50b5b12
nowhale26 <yodap321@gmail.com>
2024-11-12 21:11:50 +0300

    feat: Добавлены поля класса анализатора логов

--- src/main/java/backend/academy/LogAnalyzer.java
+++ src/main/java/backend/academy/LogAnalyzer.java
@@ -6,8 +6,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LogAnalyzer {
+    private int requestCounter=0;
+    private Map <String, Integer> mostResources;
+    private Map <String, Integer> mostCodeResponses;
+    private double averageSize=0;
+
     private static BufferedReader createReader(String path) throws IOException{
         if(path.startsWith("https://") || path.startsWith("http://")){
             URL url = URI.create(path).toURL();
@@ -15,5 +22,11 @@ public class LogAnalyzer {
         } else {
             return new BufferedReader(new FileReader(path));
         }
+    }
+
+    public void analyzeLogs(){
+        mostResources = new HashMap<>();
+        mostCodeResponses = new HashMap<>();
+
     }
 }
